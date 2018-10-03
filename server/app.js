00b3ea66cd3d0c03412e9d235119610796964696
azooz alharte <zezo50115@protonmail.com>
2018-10-03 06:14:10 +0300

    add rout to get todos by id

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,6 +25,25 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //valid id isValid
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+    //404 - send back emty array
+  Todo.findById(id).then((todoId) => {
+    if(!todoId) {
+      return res.status(404).send('id not found');
+    }
+    res.send(todoId);
+  }).catch((e) => res.status(400).send());
+
+
 });
 
 
