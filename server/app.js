8c648e1ed54c475c8e2a7fc92ea66d14c27f7a7b
azooz alharte <zezo50115@protonmail.com>
2018-12-03 06:26:24 +0300

    add patch rout /todos/:id

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb')
+///
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -64,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+    // if the body is boolean and the body is truy
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else { // if the body if not boolean or truy
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {$new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send()
+  });
+
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
